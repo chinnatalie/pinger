54222114495783de561c8a3a076fef73af5cd946
Natalie Chin <chin_natalie@yahoo.com.sg>
2020-04-08 15:52:13 +0800

    Read multi-line inputs

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const {ping} = require('./src/ping');
 const fs = require('fs');
 
 try {
-    const serverUrl = fs.readFileSync('./input.txt', 'utf8');
-    ping(serverUrl);
+    const serverUrls = fs.readFileSync('./input.txt', 'utf8').split("\n");
+    serverUrls.forEach(url => ping(url));
 } catch (err) {
     console.error('input.txt not found');
 }
