16ffd5b253ad08c112aa029cddfc2cb64bc4f975
Natalie Chin <chin_natalie@yahoo.com.sg>
2020-04-17 15:01:59 +0800

    Add validateUrl

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const {ping} = require('./src/ping');
 const fs = require('fs');
 
 try {
-    const serverUrls = fs.readFileSync('./input.txt', 'utf8').split("\n");
-    serverUrls.forEach(url => ping(url));
+    const serverUrls = fs.readFileSync('./input.txt', 'utf8').split('\n');
+    serverUrls.forEach(url => {
+        if (validateUrl(url)) ping(url);
+    });
 } catch (err) {
     console.error('input.txt not found');
+}
+
+function validateUrl(value) {
+    return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
 }
