5003ced313d9ff5d0590de97a7fb5842dcf7b56c
Jonathan Tan <jonathan.x85@gmail.com>
2020-09-03 12:31:23 +0800

    add interval ping

--- src/ping.js
+++ src/ping.js
@@ -1,13 +1,17 @@
 const request = require('request');
+const moment = require('moment');
 
 const FREQ_IN_MIN = 30;
 
 const ping = url => {
     console.log('Starting '+ FREQ_IN_MIN + '-min pings to: ' + url);
     setInterval(() => {
-        request.get({
-            url: url
-        }, () => console.log('PINGED ' + url));
+        const currMoment = moment(Date.now());
+        if(currMoment.hour() < 1 && currMoment.hour() > 8) {
+            request.get({
+                url: url
+            }, () => console.log('PINGED ' + url));
+        }
     }, FREQ_IN_MIN * 60 * 1000);
 };
 
